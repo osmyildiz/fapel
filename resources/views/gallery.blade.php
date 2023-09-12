4670359a2d6878f0abe9c74f92297c0e769d57d0
osmyildiz@gmail.com <osmyildiz@gmail.com>
2023-09-12 17:39:20 +0300

    Menu resimler

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -70,9 +70,9 @@
     </div>
 
     <!-- jQuery önce yüklenmeli -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/imagesloaded/4.1.4/imagesloaded.pkgd.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- gallery area end -->
     <script>
